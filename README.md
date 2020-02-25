ea4ee4f31f97cea0dce3cc1b7153f4062a199085
Michael Stallter <mstallter@comcast.net>
2020-02-25 11:32:41 -0600

    commit from day 2

---
+++ README.md
@@ -0,0 +1,15 @@
+# Vertical Tank Maintenance
+Website for VTM
+
+---
+1. Clone repository to local computer
+
+2. Create a new virtual environment. ```python -m venv ./venv```
+
+3. Activate the new virtual environment
+   - Windows:  ```.\venv\Scripts\activate```
+   - Mac:  ```source ./venv/bin/activate```
+
+4. Install the dependencies ```pip install -r requirements.txt```
+
+5. Start the server ```python app.py```
---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask
+from flask import render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', pageTitle='Vertical Tank Maintenance')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', pageTitle='About VTM')
+
+@app.route('/estimate', methods=['GET', 'POST'])
+def estimate():
+
+    if request.method == 'POST':
+        form = request.form
+        radius = float(form['tankRad'])
+        height = float(form['tankHt'])
+
+        #print(radius)
+        #print(height)
+
+        PI = 3.14
+        MATERIALCOST = 25
+        LABORCOST = 15
+
+        topArea = PI * radius**2
+        sideArea = (2 * (PI * (radius * height)))
+        totalAreaSqFt = (topArea + sideArea)/144 #convert from square inches to square feet
+        # print(topArea)
+        # print(sideArea)
+        # print(totalAreaSqFt)
+
+        totalMaterialCost = totalAreaSqFt * MATERIALCOST
+        totalLaborCost = totalAreaSqFt * LABORCOST
+        # print(totalMaterialCost)
+        # print(totalLaborCost)
+
+        totalBidPrice = totalMaterialCost + totalLaborCost
+        print(totalBidPrice)
+
+
+
+    return render_template('estimate.html', pageTitle='Tank Painting Estimate')
+
+if __name__ == '__main__':
+    app.run(debug=True)
